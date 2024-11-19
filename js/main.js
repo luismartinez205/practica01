a599c16d8726a36175deafcbd0eb70243cfd8d25
luismartinez205 <luisfrancisco198@gmail.com>
2024-11-19 08:36:23 -0500

    menu hamburguesa

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const navtoggle =document.querySelector(".btn-menu");
+const navmenu=document.querySelector(".container");
+navtoggle.addEventListener("click",()=>{
+navmenu.classList.toggle("container_visible");
+
+});
